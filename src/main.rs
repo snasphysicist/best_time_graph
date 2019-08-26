df12cb679c31510ea2d392d707fbbb7f6e9aed72
snasphysicist <ih8coldplay@hotmail.com>
2019-08-26 21:34:58 +0800

    Add regex, introduce datetime struct

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,23 @@
 use std::fs;
 use std::io::Error;
 
+use regex::Regex;
+
+const DATE_PATTERN : &str = r"(\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}Z)";
+
+struct DateTime {
+  year: isize,
+  month: isize,
+  day: isize,
+  hour: isize,
+  minute: isize,
+  second: isize
+}
+
+fn parse_date(date_string : &str) -> (isize, isize, isize, isize, isize, isize) {
+  let
+}
+
 fn main() {
 
   let filename : &str = "/home/scott/Documents/Projects/BestTimeGraph/test-data.txt";
