76dbb5a237af5ad58b18a33aee918655976aebea
snasphysicist <ih8coldplay@hotmail.com>
2019-08-26 20:39:22 +0800

    Read in a file

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,16 @@
 
+use std::fs;
+use std::io::Error;
+
 fn main() {
-  
+
+  let filename : &str = "/home/scott/Documents/Projects/BestTimeGraph/test-data.txt";
+
+  let read_file : Result<String, Error> = fs::read_to_string(filename);
+
+  match read_file {
+    Ok(f) => println!("{}", f),
+    Err(description) => println!("Error {}", description)
+  }
+
 }
